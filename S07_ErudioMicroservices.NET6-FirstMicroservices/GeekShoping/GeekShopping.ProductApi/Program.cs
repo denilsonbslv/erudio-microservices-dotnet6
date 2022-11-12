92282112a2a29bb53b3265ceb868206094f40d93
Denilson Silva <denilson.bslv@gmail.com>
2022-11-12 00:30:55 -0300

    Correção de dependências e inclusão da conexão com o mysql (Banco Local para teste).

--- S07_ErudioMicroservices.NET6-FirstMicroservices/GeekShoping/GeekShopping.ProductApi/Program.cs
+++ S07_ErudioMicroservices.NET6-FirstMicroservices/GeekShoping/GeekShopping.ProductApi/Program.cs
@@ -1,9 +1,16 @@
+using GeekShopping.ProductApi.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// connection with MySQL
+var connection = builder.Configuration["MySqlConnection:MysqlConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 31))));
 
 var app = builder.Build();
 
@@ -14,28 +21,4 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
